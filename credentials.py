9cddfd86f9674a116e15fefc75223cd1ac47835a
Andy Schon <amschon90@gmail.com>
2020-08-25 20:27:07 -0700

    why is it ignoring my gitignore?

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 # Insert your ACT SSO Token below.
 # Find instructions on where to access this token in the readme.
-ACT_SSO_COOKIE = ''
+ACT_SSO_COOKIE = 'MjM1ODY0MTMyNTMxODMxNjY0NjoxNTk3OTgxNTU3Mjg0OjQxZjdlZTZjNDRjYjliY2JjMmM5YTk3MDE5MzNhNTVk'
