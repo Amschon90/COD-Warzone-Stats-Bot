604ad4e38655b65b52624cbe7a79e663073fb515
Andy Schon <amschon90@gmail.com>
2020-08-30 15:01:34 -0700

    added FiNN LMG

---
+++ weapons.py
@@ -0,0 +1,71 @@
+weapons = {
+  # Assault Rifles
+  'iw8_ar_akilo47' : 'AK-47',
+  'iw8_ar_asierra12' : 'Oden',
+  'iw8_ar_falima' : 'FAL',
+  'iw8_ar_falpha' : 'FR 5.56',
+  'iw8_ar_galima' : 'CR-56 AMAX',
+  'iw8_ar_kilo433' : 'Kilo-141',
+  'iw8_ar_mcharlie' : 'M13',
+  'iw8_ar_mike4' : 'M4A1',
+  'iw8_ar_scharlie' : 'FN Scar 17',
+  'iw8_ar_sierra552' : 'Grau 5.56',
+  'iw8_ar_tango21' : 'RAM-7',
+
+  # SMGs
+  'iw8_sm_augolf' : 'AUG',
+  'iw8_sm_beta' : 'PP19 Bizon',
+  'iw8_sm_mpapa5' : 'MP5',
+  'iw8_sm_mpapa7' : 'MP7',
+  'iw8_sm_papa90' : 'P90',
+  'iw8_sm_smgolf45' : 'Striker 45',
+  'iw8_sm_uzulu' : 'Uzi',
+  'iw8_sm_victor' : 'Fennec Mk9',
+
+  # LMGs
+  'iw8_lm_kilo121' : 'M91',
+  'iw8_lm_lima86' : 'SA87',
+  'iw8_lm_mgolf34' : 'MG33',
+  'iw8_lm_mgolf36' : 'Holger-26',
+  'iw8_lm_mkilo3' : 'Bruen Mk9',
+  'iw8_lm_pkilo' : 'PKM',
+  'iw8_lm_sierrax' : 'FiNN LMG',
+
+  # Shotguns
+  'iw8_sh_dpapa12' : 'R9-0',
+  'iw8_sh_charlie725' : '725',
+  'iw8_sh_mike26' : 'VLK Rogue',
+  'iw8_sh_romeo870' : 'Model 680',
+  'iw8_sh_oscar12' : 'Origin 12',
+
+  # Marksman Rifles
+  'iw8_sn_mike14' : 'EBR-14',
+  'iw8_sn_kilo98' : 'Kar98k',
+  'iw8_sn_sbeta' : 'Mk2 Carbine',
+  'iw8_sn_sksierra' : 'SKS',
+
+  # Sniper Rifles
+  'iw8_sn_alpha50' : 'AX-50',
+  'iw8_sn_crossbow' : 'Crossbow',
+  'iw8_sn_delta' : 'Dragunov',
+  'iw8_sn_hdromeo' : 'HDR',
+  'iw8_sn_xmike109' : 'Rytec AMR',
+
+  # Launchers
+  'iw8_la_gromeo' : 'PILA',
+  'iw8_la_juliet' : 'JOKR',
+  'iw8_la_kgolf' : 'Strela-P',
+  'iw8_la_mike32' : 'MGL-32',
+  'iw8_la_rpapa7' : 'RPG-7',
+
+  # Melee
+  'iw8_me_riotshield' : 'Riot Shield',
+  
+  # Sidearms
+  'iw8_pi_cpapa' : '0.357',
+  'iw8_pi_decho' : '.50 GS',
+  'iw8_pi_golf21' : 'X16',
+  'iw8_pi_mike_1911' : '1911',
+  'iw8_pi_mike9' : 'Renetti',
+  'iw8_pi_papa320' : 'M19'
+}
