ef853940818d567c53b8135498aa0cdeb8ac29cb
Andy Schon <amschon90@gmail.com>
2020-08-30 15:01:19 -0700

    updated with available WZ game modes

---
+++ gameModes.py
@@ -0,0 +1,8 @@
+gameModes = {
+  'br_brsolos' : 'BR Solos',
+  'br_brduos' : 'BR Duos',
+  'br_brtrios' : 'BR Trios',
+  'br_brquads' : 'BR Quads',
+  'br_mini_miniroyale' : 'Mini Royale',
+  'br_kingslayer_kingsltrios' : 'Kingslayer'
+}
